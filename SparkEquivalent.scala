366bf2fd1ee0a023d6bede9235b4b283bda18c15
pujajha1 <pujajha5912@gmail.com>
2019-03-27 22:56:32 +0530

    Update SparkEquivalent.scala

--- SparkEquivalent.scala
+++ SparkEquivalent.scala
@@ -1,1 +1,8 @@
+//This is work in progress code
+val tracks:DataFrame=spark.table("raw_tracks")
+val artist:DataFrame=spark.table("raw_artist")
+val genres:DataFrame=spark.table("raw_genres")
 
+tracks
+.select("track_id")
+.withColumn("static_reporting_id",lit(1))
