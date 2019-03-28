2ad3e283bb7f4a4c8611b9b64bfb757fc27e55c2
pujajha1 <pujajha5912@gmail.com>
2019-03-28 22:35:56 +0530

    Update SparkEquivalent.scala

--- SparkEquivalent.scala
+++ SparkEquivalent.scala
@@ -1,5 +1,7 @@
 //This is work in progress code
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 val tracks:DataFrame=spark.table("raw_tracks")
 val artist:DataFrame=spark.table("raw_artist")
@@ -9,8 +11,12 @@ val rt = tracks
         .select("track_id")
         .withColumn("static_reporting_id",lit(1))
         .withColumn("epoch_new_ts",lit(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a")
-                     .parse("track_date_created").getTime()/1000))//track_date_created format='11/26/2008 01:48:14 AM'
-        .withColumn("Trending",when(tracks("track_listens")>20,"Super Hit").otherwise("Hit"))
+        .parse("track_date_created").getTime()/1000))//track_date_created format='11/26/2008 01:48:14 AM'
+        .withColumn("Trending",when(tracks("track_listens")>20,"Super Hit")
+        .otherwise("Hit"))
+        .withColumn("dt",lit(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        .format(LocalDateTime.now)))
+
 
 val ra=artist
        .withColumn("artist", trim(col("artist_name")))
