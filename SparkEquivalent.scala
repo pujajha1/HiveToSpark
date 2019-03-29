9293a25127189fb2c774b0ba6deb6ee5110332e4
pujajha1 <pujajha5912@gmail.com>
2019-03-29 16:44:45 +0530

    Update SparkEquivalent.scala

--- SparkEquivalent.scala
+++ SparkEquivalent.scala
@@ -9,6 +9,7 @@ val genres:DataFrame=spark.table("raw_genres")
 
 val rt = tracks
         .select("track_id")
+         .na.fill("NULL",Seq("person_listens"))
         .withColumn("static_reporting_id",lit(1))
         .withColumn("epoch_new_ts",lit(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a")
         .parse("track_date_created").getTime()/1000))//track_date_created format='11/26/2008 01:48:14 AM'
