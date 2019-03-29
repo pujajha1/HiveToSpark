d0b58bbbcfce6a4a7aa53aebc0cd4737057d508d
pujajha1 <pujajha5912@gmail.com>
2019-03-29 17:54:50 +0530

    Update SparkEquivalent.scala

--- SparkEquivalent.scala
+++ SparkEquivalent.scala
@@ -1,8 +1,9 @@
-//This is work in progress code
+
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+//registering different tables in Spark:
 val tracks:DataFrame=spark.table("raw_tracks")
 val artist:DataFrame=spark.table("raw_artist")
 val genres:DataFrame=spark.table("raw_genres")
@@ -11,18 +12,18 @@ val rt = tracks
         .select("track_id")
          .na.fill("NULL",Seq("person_listens"))
          .withColumnRenamed("url","track_url")
-         .na.fill(Map("col1" -> 0 , "col2" -> 0 ))
+         .na.fill(Map("col1" -> 0 , "col2" -> 0 )). //to replace null with 0
          .withColumn("static_reporting_id",lit(1))
-         .withColumn("epoch_new_ts",lit(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a")
+         .withColumn("epoch_new_ts",lit(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a") // converting to epoch
          .parse("track_date_created").getTime()/1000))//track_date_created format='11/26/2008 01:48:14 AM'
          .withColumn("Trending",when(tracks("track_listens")>20,"Super Hit")
          .otherwise("Hit"))
-         .withColumn("current_time",lit(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+         .withColumn("current_time",lit(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss") //getting current timestamp
          .format(LocalDateTime.now)))
          .filter("track_date_created='11/26/2008 01:48:14 AM'")
-         .filter("track_number<>track_id")
+         .filter("track_number<>track_id") //not equal to
          .filter("track_explicit not in ('Radio-Unsafe')")
-         .groupBy("track_id").agg(count("*").alias("cnt_track_id"),countDistinct("track_listens").alias("customers"))
+         .groupBy("track_id").agg(count("*").alias("cnt_track_id"),countDistinct("track_listens").alias("customers")) //aggregate function
 
 val ra = artist
         .withColumn("artist", trim(col("artist_name")))
@@ -46,7 +47,8 @@ val rartrg=rt
                     "cnt_track_id",
                      "customers")
 
-rartg
+// returning final Dataframe
+rartg 
 
     
 
